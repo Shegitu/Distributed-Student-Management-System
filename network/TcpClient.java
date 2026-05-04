ebc11755b8ee23b1ace2cd4e465cce8046b401dd
shegitu <physicistshegitu@gmail.com>
2026-05-04 00:11:17 -0700

    use tcp for networking, and succesfully create a multi threading users

--- network/TcpClient.java
+++ network/TcpClient.java
@@ -1,36 +1,38 @@
 package network;
+
 import java.net.*;
 import java.io.*;
-public class TcpClient {
-    
+
+public class TcpClient{
 public static void main(String[] args){
-    try{
 
-    Socket socket = new Socket("localhost", 5000);
-    System.out.println("connected to server");
+try{
 
-    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-    DataInputStream in = new DataInputStream(socket.getInputStream());
-    out.writeUTF("hello client");
-    String response = in.readUTF();
-    System.out.println(" client said "+ response);
-    in.close();
-    out.close();
-    socket.close();
-    
-    } catch(Exception e){
-        e.printStackTrace();
-    }
+Socket socket = new Socket("localhost", 5000);
+System.out.println("client connecced to server");
 
+DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+DataInputStream in = new DataInputStream(socket.getInputStream());
 
+out.writeUTF("hello ");
 
-
-
-
-    }
+String response = in.readUTF();
+System.out.println("server said " + response);
+in.close();
+out.close();
+socket.close();
+}
+catch(Exception e){
+    e.printStackTrace();
 }
 
 
 
 
 
+
+}
+
+
+
+}
